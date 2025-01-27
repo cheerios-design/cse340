8b4c6c8b627adaf043a1445cb1fd8edbb3baee8e
Sam Daramroei <samdotmc@gmail.com>
2025-01-28 01:13:48 +0300

    add inventory route and implement error handling middleware

--- server.js
+++ server.js
@@ -10,22 +10,42 @@ const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
+const inventoryRoute = require("./routes/static");
+
+const baseController = require("./controllers/baseController");
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); //not at the root
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static);
+// Index route
+app.use("/inv", inventoryRoute);
+app.get("/", baseController.buildHome);
 
-//Index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" });
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message: err.message,
+    nav,
+  });
 });
 
 /* ***********************
